f07adf25e56584db0ae43699cd169d4fe037ea4d
yunus <yunus@gmail.com>
2024-09-18 17:41:25 +0200

    User entity

--- backend/src/entity/User.ts
+++ backend/src/entity/User.ts
@@ -1,18 +1,19 @@
-import { Entity, PrimaryGeneratedColumn, Column } from "typeorm"
+import { Entity, PrimaryGeneratedColumn, Column } from 'typeorm';
+import { IsDefined, Length } from 'class-validator';
 
 @Entity()
 export class User {
-
     @PrimaryGeneratedColumn()
-    id: number
+    id: number;
 
-    @Column()
-    firstName: string
+    @Column({ type: 'varchar', length: 100, nullable: false })
+    @IsDefined({ message: "Vorname ist erforderlich." })
+    @Length(3, 100)
+    firstName!: string;
 
-    @Column()
-    lastName: string
+    @Column({ type: 'varchar', length: 100, nullable: false })
+    lastName!: string; 
 
-    @Column()
-    age: number
-
+    @Column({ type: 'varchar', length: 15, nullable: true })
+    phone?: string; // Changed phone type to string for better handling of numbers and added optional
 }
---
+++ backend/src/utility/new-user.ts
@@ -0,0 +1,20 @@
+import { AppDataSource } from "../data-source"
+import { User } from "../entity/User"
+
+
+
+export const newUser = async (payload: RegisterModel) => {
+    const user = Object.assign(new User(), payload)
+    const userRepository = AppDataSource.getRepository(User)
+
+    try {
+        const insert = await userRepository.save(user)
+        return { res: insert, status: true }
+    } catch (error) {
+        return { res: error, status: false }
+
+    }
+
+}
+
+exports.module = { newUser }
