ee2317985adef2d9653ff0241798f2e7cc1aa0f7
yunus <yunus@gmail.com>
2024-09-18 22:14:58 +0200

    entity-task.ts

--- backend/src/entity/Task.ts
+++ backend/src/entity/Task.ts
@@ -1,17 +1,52 @@
-import { Column, Entity, PrimaryGeneratedColumn } from "typeorm";
-
-
+import { AfterInsert, Column, CreateDateColumn, DeleteDateColumn, Entity, JoinColumn, ManyToOne, PrimaryGeneratedColumn, UpdateDateColumn } from "typeorm";
+import { AppDataSource } from "../data-source";
+import { User } from "./User"; // User sınıfını doğru dosyadan import edin
+import { Log } from "./Log"; // Log sınıfını doğru dosyadan import edin
+import { TaskEnum } from "./TaskEnum"; // TaskEnum sınıfını doğru dosyadan import edin
+import { TaskStatus } from "./TaskStatus"; // TaskStatus sınıfını doğru dosyadan import edin
 
 @Entity()
-
 export class Task {
 
     @PrimaryGeneratedColumn()
     id: number;
 
-    @Column({type: "enum", enum: TaskEnum, default: TaskEnum.STANDART, nullable:false})
-    type: TaskEnum
+    @Column({ type: "enum", enum: TaskEnum, default: TaskEnum.STANDART, nullable: false })
+    type: TaskEnum;
 
-    @Column({TYPE: '
-        '})
+    @Column({ type: 'varchar', length: 250, nullable: false })
+    title: string;
+
+    @Column({ type: 'text', nullable: true })
+    description: string;
+
+    @ManyToOne(() => User, (user) => user.id, { nullable: false })
+    user: User;
+
+    @ManyToOne(() => User, (user) => user.id)
+    @JoinColumn({ name: "responsibleId" })
+    responsible: User;
+
+    @Column({ type: "enum", enum: TaskStatus, default: TaskStatus.APPOINTED, nullable: false })
+    status: TaskStatus;
+
+    @CreateDateColumn()
+    createdAt: Date;
+
+    @UpdateDateColumn({ nullable: true })
+    updateAt: Date;
+
+    @DeleteDateColumn({ nullable: true })
+    deletedAt: Date;
+
+    @AfterInsert()
+    async userLog() {
+        const logRepository = AppDataSource.getRepository(Log);
+        const log = Object.assign(new Log(), {
+            type: 'task',
+            process: 'wurde task gegeben ' + this.responsible,
+            user: this.user
+        });
+        await logRepository.save(log);
+    }
 }
