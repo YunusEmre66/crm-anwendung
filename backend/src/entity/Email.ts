94bb0f8e1ce2266dac38422700e281d896077da9
yunus <yunus@gmail.com>
2024-09-18 21:30:24 +0200

    Email.ts

--- backend/src/entity/Email.ts
+++ backend/src/entity/Email.ts
@@ -1,39 +1,43 @@
-import { length } from "class-validator";
-import { AfterInsert, CreateDateColumn, DeleteDateColumn, JoinColumn, PrimaryGeneratedColumn, UpdateDateColumn } from "typeorm";
+import { Entity, Column, CreateDateColumn, DeleteDateColumn, JoinColumn, PrimaryGeneratedColumn, UpdateDateColumn, ManyToOne, AfterInsert } from "typeorm";
 import { AppDataSource } from "../data-source";
+import { User } from "./User"; // User sınıfını doğru dosyadan import edin
+import { ContactEnum } from "./ContactEnum"; // ContactEnum sınıfını doğru dosyadan import edin
+import { Log } from "./Log"; // Log sınıfını doğru dosyadan import edin
 
+@Entity()
+export class Phone {
 
-@Entity("emails")
-
-export class Email {
     @PrimaryGeneratedColumn()
     id: number;
 
     @Column({ type: "enum", enum: ContactEnum, default: ContactEnum.HOME, nullable: false })
-    emailtype: ContactEnum
+    phoneType: ContactEnum;
 
-    @Column({ type: 'varchar', length: 100, nullable: false })
-    emailAddress: string
+    @Column({ type: "varchar", length: 20, nullable: false })
+    phoneNumber: string;
 
-    @ManyToOne(() => User, (user) => user.id, {onDelete: 'CASCADE', nullable: false})
-    @JoinColumn({ name: "userId"})
-    user: User
+    @ManyToOne(() => User, (user) => user.id, { onDelete: 'CASCADE', nullable: false })
+    @JoinColumn({ name: "userId" })
+    user: User;
 
     @CreateDateColumn()
-    createAt: Date
+    createAt: Date;
 
     @UpdateDateColumn()
-    updateAt:Date
+    updateAt: Date;
 
     @DeleteDateColumn()
-    deleteAt: Date 
+    deleteAt: Date;
 
     @AfterInsert()
     async userLog() {
-        const logRepository = AppDataSource.getRepository(Log)
+        const logRepository = AppDataSource.getRepository(Log);
         const log = Object.assign(new Log(), {
-            type: 'email',
-            process: 'Address Info'
-        })
+            type: 'phone',
+            process: 'phone Info',
+            user: this.user
+        });
+
+        await logRepository.save(log);
     }
 }
