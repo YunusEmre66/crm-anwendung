2f3125e5b51302f7cb8d8318ae522359b2367280
Yunus Emre <yunuzemre30@gmail.com>
2024-09-19 11:56:40 +0200

    index.ts

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -73,23 +73,6 @@ AppDataSource.initialize().then(async () => {
     //! start express server
     app.listen(PORT)
 
-    // insert new users for test
-    await AppDataSource.manager.save(
-        AppDataSource.manager.create(User, {
-            firstName: "Timber",
-            lastName: "Saw",
-            age: 27
-        })
-    )
-
-    await AppDataSource.manager.save(
-        AppDataSource.manager.create(User, {
-            firstName: "Phantom",
-            lastName: "Assassin",
-            age: 24
-        })
-    )
-
     console.log("Express server has started on port 3000. Open http://localhost:3055/users to see results")
 
 }).catch(error => console.log(error))
