ac740b0fbf04e2f479e186fa21eb818735512448
Yunus Emre <yunuzemre30@gmail.com>
2024-09-21 21:34:38 +0200

    UserController.ts

--- backend/src/controller/UserController.ts
+++ backend/src/controller/UserController.ts
@@ -1,11 +1,12 @@
 import { AppDataSource } from "../data-source"
 import { NextFunction, Request, Response } from "express"
 import { User } from "../entity/User"
+import { UserModel } from "../model/UserModel"
+import { RegisterModel } from "../model/RegisterModel"
 import { Email } from "../entity/Email"
 import { Phone } from "../entity/Phone"
 import { Address } from "../entity/Address"
-import { UserModel } from "../model/UserModel"
-
+import { CreatedAt } from "sequelize-typescript"
 
 export class UserController {
 
@@ -15,15 +16,27 @@ export class UserController {
     private addressRepository = AppDataSource.getRepository(Address)
 
 
+    //! all
     async all(request: Request, response: Response, next: NextFunction) {
-        //! buradan devam
+        const users: Array<UserModel> = (await this.userRepository.find()).map((k: UserModel) => {
+            return {
+                id: k.id,
+                firstName: k.firstName,
+                lastName: k.lastName,
+                email: k.email,
+                role: k.role,
+                confirmed: k.confirmed,
+                createdAt: k.createdAt
+            } as UserModel  //! modelleri girdikten sonra k. içindeki veriler hazır olarak dönüyor.  
+        })
+
+        return { status: true, data: users }
     }
 
-    //! all
+    //!one
 
     async one(request: Request, response: Response, next: NextFunction) {
         const id = parseInt(request.params.id)
-
 
         const user = await this.userRepository.findOne({
             where: { id }
@@ -32,22 +45,31 @@ export class UserController {
         if (!user) {
             return "unregistered user"
         }
-        return user
+        return {
+            status: true, data: {
+                firstName: user.firstName, lastName: user.lastName,
+                email: user.email,
+                role: user.role,
+                confirmed: user.confirmed,
+                createdAt: user.createdAt
+            } as UserModel
+        }
     }
-
+    //!save
     async save(request: Request, response: Response, next: NextFunction) {
-        const { firstName, lastName, age } = request.body;
+        const { firstName, lastName, email, password }: RegisterModel = request.body;
 
         const user = Object.assign(new User(), {
             firstName,
             lastName,
-            age
+            email,
+            password
         })
 
         return this.userRepository.save(user)
     }
-
-    async remove(request: Request, response: Response, next: NextFunction) {
+    //! newUser
+    async newUser(request: Request, response: Response, next: NextFunction) {
         const id = parseInt(request.params.id)
 
         let userToRemove = await this.userRepository.findOneBy({ id })
