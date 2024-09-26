7c1bdef76918932a3d53e05da61c1473e7e88b48
Yunus Emre <yunuzemre30@gmail.com>
2024-09-26 10:34:24 +0200

    authcontext

--- frontend/src/pages/_app.tsx
+++ frontend/src/pages/_app.tsx
@@ -2,7 +2,8 @@ import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 import { Provider } from "react-redux";  //! provider demek : bir componentin altındaki tüm componentlere store'u geçirir.
 import { store } from "@/store"; //! store demek : redux store'u demektir. yani tüm state'lerin tutulduğu yer.
-import { AuthProvider } from "@/context/auth-context"; //! AuthProvider demek : bir context provider'ıdır. yani bir componentin altındaki tüm componentlere context'i geçirir.
+import { AuthProvider } from './../../src/context/auth-context';
+ //! AuthProvider demek : bir context provider'ıdır. yani bir componentin altındaki tüm componentlere context'i geçirir.
 
 export default function App({ Component, pageProps }: AppProps) {
     return (
