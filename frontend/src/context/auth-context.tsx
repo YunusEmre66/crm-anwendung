6181ed0f19f9db008ca6afe902c0429a792d44de
Yunus Emre <yunuzemre30@gmail.com>
2024-09-28 10:18:12 +0200

    authcontext

--- frontend/src/context/auth-context.tsx
+++ frontend/src/context/auth-context.tsx
@@ -1,28 +1,45 @@
 // import { AppDispatch } from "@/store";
-import React, { ReactNode, createContext, useContext } from "react";
+import React, { ReactNode, createContext, useContext, useEffect } from "react";
+import { useDispatch } from "react-redux";
+import { getIsLogin, logout } from "@/store/apps/login";
 import { useRouter } from "next/router";
 
 // ** Defaults
-
 const defaultProvider: any = {
-    user: null,
+  user: null,
 };
 
 type Props = {
-    children: ReactNode;
-}
+  children: ReactNode;
+};
 
 const AuthContext = createContext(defaultProvider);
 
 export function AuthProvider({ children }: Props) {
-    const router = useRouter();
-    const sharedData = "Context API Kullanımı";
-    return (
-        <AuthContext.Provider value={sharedData}>{children}</AuthContext.Provider>
-      );
+  // ** Redux
+  // const dispatch = useDispatch<AppDispatch>();
+
+  const router = useRouter();
+
+  // useEffect(() => {
+  //   if (router.asPath === "/logout") {
+  //     dispatch(logout());
+  //     router.push('/login')
+  //   }
+  // }, [router, dispatch]);
+
+  // useEffect(() => {
+  //   dispatch(getIsLogin());
+  // }, [dispatch, router]);
+
+  const sharedData = "Context API Kullanımı";
+
+  return (
+    <AuthContext.Provider value={sharedData}>{children}</AuthContext.Provider>
+  );
 }
 
+// Consumer Hook
 export function useAuthContext() {
-    return useContext(AuthContext);
-  }
-  
+  return useContext(AuthContext);
+}
